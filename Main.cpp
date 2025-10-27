71ff2d4ee18b59fcd6f4fd05f19a3f88f04b17af
Founder-TGL <ginsbergaiden@gmail.com>
2025-10-27 23:56:53 +0000

    booom

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,16 @@
+#include <ctime>
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    clock_t before, now, delta;
+    before = clock();
+    while(true){
+        now = clock();
+        delta = now-before;
+        cout << "delta: "<< (float)delta << "\n";
+        before = now;
+    }
+
+}
